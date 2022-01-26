b8c5454576cad61e7f76ee8aa447e58eadb6e130
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2022-01-26 15:15:06 +0500

    Created reactapp

---
+++ myapp1/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <>
+  <nav>
+    <li>Home</li>
+    <li>Menu</li>
+    <li>Order</li>
+    <li>About us</li>
+    
+  </nav>
+  </>  
+);
+}
+
+export default App;
