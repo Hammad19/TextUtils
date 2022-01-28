db1ab72eb6937080b4d7a0c6a9558b6a29d7ba9e
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2022-01-28 16:33:38 +0500

    Changed Navbar

--- myapp1/src/components/Navbar.js
+++ myapp1/src/components/Navbar.js
@@ -5,25 +5,25 @@ import TextForm from './TextForm';
 
 export default function Navbar(props) {
   return (
-    <div className="wrapper">
-    <header>
-       <nav>
-          <div className="menu-icon">
-             <i className="fa fa-bars fa-2x"></i>
-          </div>
-          <div className="logo">
-             {props.title}
-          </div>
-          <div className="menu">
-             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                
-             </ul>
-          </div>
-       </nav>
-    </header>
-    </div>
+   <nav className="navbar navbar-expand-lg navbar-light bg-light">
+   <div className="container-fluid">
+     <a className="navbar-brand" href="#">{props.title}</a>
+     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+         <li className="nav-item">
+           <a className="nav-link active" aria-current="page" href="#">Home</a>
+         </li>
+         <li className="nav-item">
+           <a className="nav-link" href="#">About</a>
+         </li>
+       </ul>
+       <form className="d-flex">
+         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+         <button className="btn btn-outline-success" type="submit">Search</button>
+       </form>
+     </div>
+   </div>
+ </nav>
     );
 }
 
--- myapp1/src/components/TextForm.js
+++ myapp1/src/components/TextForm.js
@@ -4,6 +4,41 @@ import '../index.css';
 export default function TextForm(props) {
 
   const [text,SetText] = useState("Enter Text");
+
+  const [btntext,Setbtntext] = useState("Enable Dark Mode");
+
+  const [myStyle,setmyStyle] = useState(
+    {
+
+    color: 'white',
+    backgroundColor: 'black'
+  }
+  );
+
+  const togglestyle = () =>
+  {
+    if (myStyle.color === 'black')
+    {
+      setmyStyle(
+        {
+          color: 'white',
+          backgroundColor: 'black'
+        }
+      )
+      Setbtntext('Enable Light Mode');
+    }
+    else
+    {
+
+      setmyStyle(
+        {
+          color: 'black',
+          backgroundColor: 'white'
+        }
+      )
+      Setbtntext('Enable Dark Mode');
+    }
+  }
   const [count,SetCount] = useState(0);
 
   const handleOnChange = (event) => {
@@ -39,13 +74,17 @@ export default function TextForm(props) {
 
   return (
     <>
-    <div className="container my-3">
-    <h1 className=''>
+    <div className='.hei' style= {myStyle}>
+    <div className="container">
+    <h1 className='mg-4'>
+      <br></br>
       {props.title}
+     
     </h1>
+    <br></br>
     <div className="input-group mb-3">
     
-      <textarea className="form-control" rows={8} value={text} onChange={handleOnChange}>
+      <textarea style={myStyle} className="form-control" rows={8} value={text} onChange={handleOnChange}>
       </textarea>
     </div>
     </div>
@@ -54,14 +93,20 @@ export default function TextForm(props) {
     <button type="button" className="btn btn-primary mx-2" onClick={toLowerCase}>Get Into LowerCase</button>
     <button type="button" className="btn btn-primary mx-2 " onClick={clearText}>Clear Text</button>
     <button type="button" className="btn btn-primary mx-2 " onClick={removeExtraSpaces}>Remove Extra Spaces</button>
+    <button type="button" className="btn btn-primary mx-2 " onClick={togglestyle}>{btntext}</button>
     </div>
     
-    <div className='container my-3'>
+    <div className='container my-5'>
       <h1>
         Your Text Summary
       </h1>
+    
       <p>{count} word and {text.length} characters</p>
       <p>{0.008 * text.split(' ').length} Minutes read</p>
+    </div>
+    <br></br>
+    <br></br>
+    
     </div>
     </>
     );
