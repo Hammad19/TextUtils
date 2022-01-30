52597f0586b5db007c099dd430af704f7a6e1c2e
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2022-01-31 00:31:09 +0500

    Update Navbar.js

--- myapp1/src/components/Navbar.js
+++ myapp1/src/components/Navbar.js
@@ -9,7 +9,10 @@ export default function Navbar(props) {
    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
    <div className="container-fluid">
      <a className="navbar-brand" href="/">{props.title}</a>
-     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
          <li className="nav-item">
            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
